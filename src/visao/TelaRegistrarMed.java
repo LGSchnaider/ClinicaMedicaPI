e225a3852fa5e2004f1c947de25723f259428715
Marcos D Lopes <marcos.dl@aluno.ifsc.edu.br>
2023-02-07 12:04:10 -0300

    Commit Marcos
    Telas

---
+++ src/visao/TelaRegistrarMed.java
@@ -0,0 +1,41 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TelaRegistrarMed extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaRegistrarMed frame = new TelaRegistrarMed();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaRegistrarMed() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
--- src/visao/TelaRegistrarPaciente.java
+++ src/visao/TelaRegistrarPaciente.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class MedicoTela extends JFrame {
+public class TelaRegistrarPaciente extends JFrame {
 
 	private JPanel contentPane;
 
@@ -17,7 +17,7 @@ public class MedicoTela extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MedicoTela frame = new MedicoTela();
+					TelaRegistrarPaciente frame = new TelaRegistrarPaciente();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -29,7 +29,7 @@ public class MedicoTela extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MedicoTela() {
+	public TelaRegistrarPaciente() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
