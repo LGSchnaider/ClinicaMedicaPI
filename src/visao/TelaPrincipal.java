4edb5d33f60d220750e4d02659bf3c8cd2494e38
Luiz Gustavo Schnaider <luizgustavoschnaider@gmail.com>
2023-02-14 11:46:45 -0300

    Designer1

--- src/visao/TelaPrincipal.java
+++ src/visao/TelaPrincipal.java
@@ -5,10 +5,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.Color;
 
 public class TelaPrincipal extends JFrame {
 
 	private JPanel contentPane;
+	private JTable visaoGeral;
 
 	/**
 	 * Launch the application.
@@ -31,11 +43,198 @@ public class TelaPrincipal extends JFrame {
 	 */
 	public TelaPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelDireita = new JPanel();
+		contentPane.add(panelDireita, BorderLayout.WEST);
+		panelDireita.setLayout(new BorderLayout(0, 0));
+		
+		JPanel Norte = new JPanel();
+		Norte.setBackground(new Color(184, 180, 7));
+		panelDireita.add(Norte, BorderLayout.NORTH);
+		Norte.setLayout(new BorderLayout(0, 0));
+		
+		JLabel TituloVisaoGeral = new JLabel("Visão Geral");
+		TituloVisaoGeral.setBackground(new Color(184, 180, 7));
+		TituloVisaoGeral.setFont(new Font("Times New Roman", Font.BOLD, 40));
+		Norte.add(TituloVisaoGeral);
+		
+		JPanel sul = new JPanel();
+		sul.setBackground(new Color(64, 128, 128));
+		panelDireita.add(sul, BorderLayout.SOUTH);
+		
+		JPanel lest = new JPanel();
+		lest.setBackground(new Color(64, 128, 128));
+		panelDireita.add(lest, BorderLayout.WEST);
+		
+		JPanel oeste = new JPanel();
+		oeste.setBackground(new Color(64, 128, 128));
+		panelDireita.add(oeste, BorderLayout.EAST);
+		
+		JPanel centro = new JPanel();
+		panelDireita.add(centro, BorderLayout.CENTER);
+
+		visaoGeral = new JTable();
+		visaoGeral.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Oracio", "Jorge", "10"},
+			},
+			new String[] {
+				"Paciente", "M\u00E9dico", "Hora"
+			}
+		));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		centro.add(scrollPane);
+		
+		scrollPane.add(visaoGeral);
+		
+		JPanel panelCentro = new JPanel();
+		contentPane.add(panelCentro, BorderLayout.CENTER);
+		panelCentro.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(64, 128, 128));
+		panelCentro.add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("Painel de controle");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(64, 128, 128));
+		panelCentro.add(panel_1, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Médicos:");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 1;
+		panel_1.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 2;
+		panel_1.add(btnNewButton, gbc_btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Lista de Médicos");
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_1.gridx = 3;
+		gbc_btnNewButton_1.gridy = 2;
+		panel_1.add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 1;
+		gbc_lblNewLabel_3.gridy = 3;
+		panel_1.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		JLabel lblNewLabel_2 = new JLabel("Paciente");
+		lblNewLabel_2.setForeground(new Color(0, 0, 0));
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 4;
+		panel_1.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JButton btnNewButton_2 = new JButton("Cadastrar");
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_2.gridx = 1;
+		gbc_btnNewButton_2.gridy = 5;
+		panel_1.add(btnNewButton_2, gbc_btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Lista de Pacientes");
+		btnNewButton_3.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
+		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_3.gridx = 3;
+		gbc_btnNewButton_3.gridy = 5;
+		panel_1.add(btnNewButton_3, gbc_btnNewButton_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 1;
+		gbc_lblNewLabel_4.gridy = 6;
+		panel_1.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Consultas:");
+		lblNewLabel_5.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 1;
+		gbc_lblNewLabel_5.gridy = 7;
+		panel_1.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		
+		JButton btnNewButton_4 = new JButton("Cadastrar");
+		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
+		gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_4.gridx = 1;
+		gbc_btnNewButton_4.gridy = 8;
+		panel_1.add(btnNewButton_4, gbc_btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Consultas");
+		btnNewButton_5.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
+		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_5.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnNewButton_5.gridx = 3;
+		gbc_btnNewButton_5.gridy = 8;
+		panel_1.add(btnNewButton_5, gbc_btnNewButton_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 1;
+		gbc_lblNewLabel_6.gridy = 9;
+		panel_1.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("Secretários");
+		lblNewLabel_7.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_7.gridx = 1;
+		gbc_lblNewLabel_7.gridy = 10;
+		panel_1.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		JButton btnNewButton_6 = new JButton("Cadastrar");
+		btnNewButton_6.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		GridBagConstraints gbc_btnNewButton_6 = new GridBagConstraints();
+		gbc_btnNewButton_6.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_6.gridx = 1;
+		gbc_btnNewButton_6.gridy = 11;
+		panel_1.add(btnNewButton_6, gbc_btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("Listar secretários");
+		btnNewButton_7.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		GridBagConstraints gbc_btnNewButton_7 = new GridBagConstraints();
+		gbc_btnNewButton_7.gridx = 3;
+		gbc_btnNewButton_7.gridy = 11;
+		panel_1.add(btnNewButton_7, gbc_btnNewButton_7);
 	}
 
 }
