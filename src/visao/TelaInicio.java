179b72a0f7a03b6bcd9a6e6228768c30ad543cba
Marcos D Lopes <marcos.dl@aluno.ifsc.edu.br>
2023-02-14 11:37:03 -0300

    Commit Marcos Tela Inicio
    Redimenções feitas

--- src/visao/TelaInicio.java
+++ src/visao/TelaInicio.java
@@ -22,8 +22,12 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class TelaInicio extends JFrame {
+	private JTextField textFieldLog;
+	private JTextField textFieldSenha;
 
 	/**
 	 * Launch the application.
@@ -62,20 +66,23 @@ public class TelaInicio extends JFrame {
 		down.add(lblNewLabel);
 		
 		JPanel center = new JPanel();
+		center.setBackground(new Color(0, 128, 128));
+		center.setForeground(new Color(0, 128, 128));
 		getContentPane().add(center, BorderLayout.CENTER);
-		center.setLayout(new MigLayout("", "[310.00,grow][223.00,grow][330.00,grow]", "[pref!,grow][92.00,grow][40,grow][70,grow][70,grow][70,grow][38.00,grow]"));
+		center.setLayout(new MigLayout("", "[209.00][246.00,grow][][254.00,grow][205.00,grow][330.00]", "[pref!,grow][92.00][133.00][70,grow][83.00][96.00][38.00,grow]"));
 		
 		JLabel lblNewLabel_1 = new JLabel("Clínica Médica");
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		lblNewLabel_1.setBackground(new Color(255, 0, 0));
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 60));
 		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
-		center.add(lblNewLabel_1, "cell 1 1,alignx center");
+		center.add(lblNewLabel_1, "cell 3 1,alignx center");
 		
 		JLabel lblNewLabel_2 = new JLabel("INICIO");
-		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 15));
-		center.add(lblNewLabel_2, "cell 1 2,alignx center");
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 40));
+		center.add(lblNewLabel_2, "cell 3 2,alignx center");
 		
 		JPanel LOG = new JPanel();
-		LOG.setLayout(new GridLayout(1, 1, 0, 0));
+		LOG.setBackground(new Color(0, 128, 128));
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -87,29 +94,43 @@ public class TelaInicio extends JFrame {
 				// TERMINA O CHAMADO
 			}
 		});
-		LOG.add(btnCadastrar);
 		
 		btnCadastrar.setMinimumSize(new Dimension(97, 23));
 		btnCadastrar.setMaximumSize(new Dimension(97, 23));
 		
 		
-		center.add(LOG, "cell 1 3,alignx center");
+		center.add(LOG, "cell 3 3,alignx center");
+		GroupLayout gl_LOG = new GroupLayout(LOG);
+		gl_LOG.setHorizontalGroup(
+			gl_LOG.createParallelGroup(Alignment.LEADING)
+				.addComponent(btnCadastrar, GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
+		);
+		gl_LOG.setVerticalGroup(
+			gl_LOG.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_LOG.createSequentialGroup()
+					.addComponent(btnCadastrar, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(25, Short.MAX_VALUE))
+		);
+		LOG.setLayout(gl_LOG);
 		
-		JButton btnLogin = new JButton("Login");
-		center.add(btnLogin, "cell 1 4,alignx center");
-		btnLogin.addActionListener(new ActionListener (){
-			public void actionPerformed(ActionEvent e) {
-					dispose();
-					// CHAMA A CLASSE LOGIN
-					TelaLogin frame = new TelaLogin();
-					frame.setLocationRelativeTo(null);
-					frame.setVisible(true);
-					// TERMINA O CHAMADO
-				}
-			});
+		JLabel lblLogin = new JLabel("Login:");
+		lblLogin.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		center.add(lblLogin, "cell 2 4,alignx trailing");
+		
+		textFieldLog = new JTextField();
+		center.add(textFieldLog, "cell 3 4,growx,aligny center");
+		textFieldLog.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		center.add(lblSenha, "cell 2 5,alignx trailing");
+		
+		textFieldSenha = new JTextField();
+		center.add(textFieldSenha, "cell 3 5,growx");
+		textFieldSenha.setColumns(10);
 		
 		JButton btnLogOff = new JButton("Sair");
-		center.add(btnLogOff, "cell 1 5,alignx center");
+		center.add(btnLogOff, "cell 3 6,alignx center");
 		btnLogOff.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
