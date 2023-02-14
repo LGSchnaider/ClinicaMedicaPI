103b3577578ed9646e6ca545a6b894286c5b0ae3
Luiz Gustavo Schnaider <luizgustavoschnaider@gmail.com>
2023-02-14 09:57:57 -0300

    Correção de erro

--- src/controle/AdministradorDAO.java
+++ src/controle/AdministradorDAO.java
@@ -10,9 +10,7 @@ public class AdministradorDAO implements IAdministradorDAO {
 	private static ArrayList<Administrador> produtos;
 	private static AdministradorDAO instancia;
 
-	/**
-	 * Padrao singleton
-	 */
+
 	private AdministradorDAO() {
 	}
 
@@ -21,25 +19,6 @@ public class AdministradorDAO implements IAdministradorDAO {
 		if (instancia == null) {
 			instancia = new AdministradorDAO();
 			produtos = new ArrayList<>();
-
-			Administrador p1 = new Administrador();
-			p1.setNome("Bola");
-			p1.setCategoria("Brinquedo");
-			p1.setValor(30.00);
-
-			Administrador p2 = new Administrador();
-			p2.setNome("Carrinho de controle remoto");
-			p2.setCategoria("Brinquedo");
-			p2.setValor(120.00);
-
-			Administrador p3 = new Administrador();
-			p3.setNome("Kinder ovo");
-			p3.setCategoria("Alimento");
-			p3.setValor(8.00);
-
-			produtos.add(p1);
-			produtos.add(p2);
-			produtos.add(p3);
 		}
 
 		return instancia;
