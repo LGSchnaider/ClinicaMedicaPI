699c71bb6065e44d7e7783a86c7166f5c29247c1
Marcos D Lopes <marcosdlopes21@gmail.com>
2023-03-21 10:04:19 -0300

    TelaCadP
    feita

--- src/visao/TelaCadConsulta.java
+++ src/visao/TelaCadConsulta.java
@@ -19,29 +19,10 @@ import java.awt.TextArea;
 public class TelaCadConsulta extends JFrame {
 
 	private JPanel contentPane;
-	private final JButton btnNewButton_1 = new JButton("Cadastrar");
-	private JTextField textField;
-	private JTextField textField_1;
+	private final JButton btnCad = new JButton("Cadastrar");
+	private JTextField txtDoenca;
+	private JTextField txtValor;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaCadConsulta frame = new TelaCadConsulta();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public TelaCadConsulta() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 816, 562);
@@ -49,14 +30,14 @@ public class TelaCadConsulta extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[grow,right][grow][grow]", "[grow][grow][grow][grow][grow][grow][91.00,grow][grow][grow]"));
+		contentPane.setLayout(new MigLayout("", "[grow,right][grow][grow]", "[grow][grow][grow][grow][grow][grow][110.00][grow][grow][grow]"));
 		
 		JLabel lblNewLabel = new JLabel("Cadastrar Consulta");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 40));
 		contentPane.add(lblNewLabel, "cell 1 0,alignx center");
 		
-		JButton btnNewButton = new JButton("Voltar");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnVolta = new JButton("Voltar");
+		btnVolta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 				TelaPrincipal frame = new TelaPrincipal();
@@ -66,11 +47,11 @@ public class TelaCadConsulta extends JFrame {
 			}
 		});
 		
-		JLabel lblNewLabel_6 = new JLabel("Paciente");
+		JLabel lblNewLabel_6 = new JLabel("Paciente:");
 		contentPane.add(lblNewLabel_6, "cell 0 2");
 		
-		Choice choice = new Choice();
-		contentPane.add(choice, "cell 1 2,growx");
+		Choice chPaci = new Choice();
+		contentPane.add(chPaci, "cell 1 2,growx");
 		
 		JLabel lblNewLabel_1 = new JLabel("Data:");
 		contentPane.add(lblNewLabel_1, "cell 0 3");
@@ -79,14 +60,14 @@ public class TelaCadConsulta extends JFrame {
 		contentPane.add(panel, "cell 1 3,grow");
 		panel.setLayout(new MigLayout("", "[grow][grow][grow]", "[][grow]"));
 		
-		JComboBox comboBox = new JComboBox();
-		panel.add(comboBox, "cell 0 0,growx");
+		JComboBox cbDia = new JComboBox();
+		panel.add(cbDia, "cell 0 0,growx");
 		
-		JComboBox comboBox_2 = new JComboBox();
-		panel.add(comboBox_2, "cell 1 0,growx");
+		JComboBox cbMes = new JComboBox();
+		panel.add(cbMes, "cell 1 0,growx");
 		
-		JComboBox comboBox_3 = new JComboBox();
-		panel.add(comboBox_3, "cell 2 0,growx");
+		JComboBox cbAno = new JComboBox();
+		panel.add(cbAno, "cell 2 0,growx");
 		
 		JLabel lblNewLabel_2 = new JLabel("Hora:");
 		contentPane.add(lblNewLabel_2, "cell 0 4");
@@ -95,11 +76,11 @@ public class TelaCadConsulta extends JFrame {
 		contentPane.add(panel_1, "cell 1 4,growx");
 		panel_1.setLayout(new MigLayout("", "[30px,grow,fill][grow]", "[22px,grow,fill]"));
 		
-		JComboBox comboBox_1 = new JComboBox();
-		panel_1.add(comboBox_1, "cell 0 0");
+		JComboBox cbHora = new JComboBox();
+		panel_1.add(cbHora, "cell 0 0");
 		
-		JComboBox comboBox_4 = new JComboBox();
-		panel_1.add(comboBox_4, "cell 1 0,growx");
+		JComboBox cbMin = new JComboBox();
+		panel_1.add(cbMin, "cell 1 0,growx");
 		
 		JPanel panel_3 = new JPanel();
 		contentPane.add(panel_3, "cell 0 5,grow");
@@ -115,26 +96,26 @@ public class TelaCadConsulta extends JFrame {
 		contentPane.add(panel_2, "cell 1 5,grow");
 		panel_2.setLayout(new MigLayout("", "[grow]", "[grow][grow]"));
 		
-		textField = new JTextField();
-		panel_2.add(textField, "cell 0 0,growx");
-		textField.setColumns(10);
+		txtDoenca = new JTextField();
+		panel_2.add(txtDoenca, "cell 0 0,growx");
+		txtDoenca.setColumns(10);
 		
-		textField_1 = new JTextField();
-		panel_2.add(textField_1, "cell 0 1,growx");
-		textField_1.setColumns(10);
+		txtValor = new JTextField();
+		panel_2.add(txtValor, "cell 0 1,growx");
+		txtValor.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("Descrição:");
 		contentPane.add(lblNewLabel_5, "cell 0 6,alignx right,aligny center");
 		
-		TextArea textArea = new TextArea();
-		contentPane.add(textArea, "cell 1 6,growx");
-		contentPane.add(btnNewButton, "cell 0 7,alignx center,aligny baseline");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		TextArea txaDesc = new TextArea();
+		contentPane.add(txaDesc, "cell 1 6,growx,aligny center");
+		contentPane.add(btnVolta, "cell 0 8,alignx center,aligny center");
+		btnCad.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
-		contentPane.add(btnNewButton_1, "cell 2 7,alignx center,aligny baseline");
+		contentPane.add(btnCad, "cell 2 8,alignx center,aligny center");
 	}
 
 }
