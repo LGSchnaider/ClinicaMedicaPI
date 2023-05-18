96e770dce48274915a4a2945eb67cdb86fa99dbf
Luiz.g10 <luizgustavoschnaider@gmail.com>
2023-05-18 08:40:14 -0300

    Uhull2.0

--- src/test/MedicoTest.java
+++ src/test/MedicoTest.java
@@ -23,10 +23,9 @@ class MedicoTest {
 		//chama conexão
 		Medico m = new Medico();
 		m.setNome("Dr.Lucas pereira");
-		m.setCpf((long) 262532655);
+		m.setCpf((long) 751542154);
 		m.setUf("AC");
-		m.setCrm((long) 545158);
-		m.getUsuario().setIdusuario(15);
+		m.setCrm((long) 954828);
 		m.getUsuario().setLogin("BlaBla");
 		m.getUsuario().setSenha("blabla");
 		m.getUsuario().setPefil(1);
@@ -34,6 +33,11 @@ class MedicoTest {
 		udao.inserir(m.getUsuario());
         mdDao.inserir(m);
 		assertEquals("Dr.Lucas pereira", m.getNome());
-		
+		assertEquals(751542154, m.getCpf());
+		assertEquals("AC", m.getUf());
+		assertEquals(954828, m.getCrm());
+		assertEquals("BlaBla", m.getUsuario().getLogin());
+		assertEquals("blabla", m.getUsuario().getSenha());
+		assertEquals(1, m.getUsuario().getPerfil());
 	}
 }
