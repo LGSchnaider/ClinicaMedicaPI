3ba0d7d2c02a38a249ae4ccd27bc0384a4c4df7e
Unknown <unknown@example.com>
2023-06-13 14:42:34 -0300

    PDS Feito

--- src/main/java/controll/CConnection.java
+++ src/main/java/controll/CConnection.java
@@ -1,5 +1,8 @@
 package controll;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,17 +11,40 @@ public class CConnection {
 
 	private static Connection conexao;
 	private static CConnection instancia;
-	private static final String DATABASE = "ClinicaPi";
-	private static final String USER = "root";
-	private static final String PSW = "aluno";
+
+	private static String DATABASE;
+	private static String USER;
+	private static String PSW;
 
 	private CConnection() {
 
 	}
 
+	public static void leArquivoBD(String filename) {
+		String nomeBanco = null, username = null, password = null;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			if (reader != null) {
+				nomeBanco = reader.readLine();
+				username = reader.readLine();
+				password = reader.readLine();
+			}
+			reader.close();
+
+			DATABASE = nomeBanco;
+			USER = username;
+			PSW = password;
+
+		} catch (IOException e) {
+			System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+			return;
+		}
+	}
+
 	public static CConnection getInstancia() {
 		if (instancia == null) {
 			instancia = new CConnection();
+			leArquivoBD("credentials.txt");
 		}
 		return instancia;
 	}
